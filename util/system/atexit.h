aafdc22c244507dc6f27d162ec4338be420f20bb
kizill <staskirillof@yandex.ru>
2020-03-22 22:01:50 +0300

    fix issue #803 - call _catboost.pyd atexit callbacks on python interpreter level atexit event

--- util/system/atexit.h
+++ util/system/atexit.h
@@ -11,4 +11,9 @@ void AtExit(TAtExitFunc func, void* ctx, size_t priority);
 void AtExit(TTraditionalAtExitFunc func);
 void AtExit(TTraditionalAtExitFunc func, size_t priority);
 
+/**
+ * This method only for very special cases, like graceful python DLL module unload
+ */
+void ManualRunAtExitFinalizers();
+
 bool ExitStarted();
